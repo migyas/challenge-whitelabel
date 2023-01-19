d564664ca5787ccf0d33495db3a0ab550204b460
Yan Dias <yansantos0712@gmail.com>
2023-01-19 16:00:31 -0300

    feat: users screen in progress

--- src/pages/Users/index.tsx
+++ src/pages/Users/index.tsx
@@ -1,3 +1,69 @@
+import { useEffect, useState } from "react";
+import { getAllUsers } from "../../services/v1/user-service";
+import { Container, UsersList } from "./styles";
+
+interface User {
+  nivel: string;
+  nome: string;
+  telefone: string;
+  corDeFundo: string;
+  email: string;
+  senha: string;
+}
+
+interface VariantRole {
+  [key: string]: string;
+}
+
 export default function Users() {
-  return <h1>Users</h1>;
+  const [users, setUsers] = useState<User[]>([]);
+
+  const VARIANTS_ROLES: VariantRole = {
+    operator: "Lojista",
+    admin: "Admin",
+  };
+
+  async function getUsers() {
+    const data = await getAllUsers();
+    setUsers(data);
+  }
+
+  useEffect(() => {
+    (async () => {
+      await getUsers();
+    })();
+  }, []);
+
+  return (
+    <Container>
+      <h1>Usuários</h1>
+      <div>
+        <button>Adicionar Usuário</button>
+      </div>
+      <UsersList>
+        <table>
+          <thead>
+            <tr>
+              <th>Nome</th>
+              <th>Telefone</th>
+              <th>E-mail</th>
+              <th>Nível</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => {
+              return (
+                <tr key={user.email}>
+                  <td>{user.nome}</td>
+                  <td>{user.telefone}</td>
+                  <td>{user.email}</td>
+                  <td>{VARIANTS_ROLES[user.nivel]}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </UsersList>
+    </Container>
+  );
 }
