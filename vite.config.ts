24ec72508a4ca000024983c5bd847a748181317e
Yan Dias <yansantos0712@gmail.com>
2023-01-21 19:53:40 -0300

    fix: env

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPath from 'vite-tsconfig-paths';
+import {viteExternalsPlugin} from 'vite-plugin-externals';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPath()],
+  plugins: [
+    react(),
+    tsconfigPath(),
+    viteExternalsPlugin({
+      vue: 'Vue',
+      react: 'React',
+      'react-dom': 'ReactDOM',
+      // value support chain, transform to window['React']['lazy']
+      lazy: ['React', 'lazy'],
+    }),
+  ],
   envPrefix: 'REACT_',
   server: {
     port: 3000,
