f6c2307ccab29013913c2143b53a2079cc05b685
Yan Dias <yansantos0712@gmail.com>
2023-01-21 19:59:12 -0300

    ..

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,14 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPath from 'vite-tsconfig-paths';
-import {viteExternalsPlugin} from 'vite-plugin-externals';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPath(),
-    viteExternalsPlugin({
-      vue: 'Vue',
-      react: 'React',
-      'react-dom': 'ReactDOM',
-      // value support chain, transform to window['React']['lazy']
-      lazy: ['React', 'lazy'],
-    }),
-  ],
+  plugins: [react(), tsconfigPath()],
   server: {
     port: 3000,
   },
-  define: {
-    global: {},
-  },
+
   preview: {
     host: '0.0.0.0',
     port: 3333,
