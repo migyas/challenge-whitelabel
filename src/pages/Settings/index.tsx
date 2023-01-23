c8980829258340d072259c07978d4915e3406207
Yan Dias <yansantos0712@gmail.com>
2023-01-23 16:32:14 -0300

    feat: settings options background color

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -1,10 +1,66 @@
 import animatePresence from '@/components/AnimatePresence';
-import {Container} from './styles';
+import {Button} from '@/components/Button';
+import {Select} from '@/components/Select';
+import useUser from '@/hooks/useUser';
+import {optionsBackgroundColor} from '@/utils/mocks/optionsBackgroundColor';
+import {useEffect} from 'react';
+import {Controller, useForm} from 'react-hook-form';
+import {Container, FormContainer} from './styles';
+
+interface SettingsProps {
+  corDeFundo: {
+    label: string;
+    value: string;
+  };
+}
 
 function Settings() {
+  const {control, handleSubmit, reset} = useForm<SettingsProps>();
+  const {userLogged} = useUser();
+
+  function getCorDeFundoOption(value: string) {
+    const findCorDeFundo = optionsBackgroundColor.find(
+      (option) => option.value === value,
+    );
+    return findCorDeFundo;
+  }
+
+  function onSubmit({corDeFundo}: SettingsProps) {
+    const updateCorDeFundo = {
+      ...userLogged,
+      corDeFundo: corDeFundo.value,
+    };
+    localStorage.setItem('login', JSON.stringify(updateCorDeFundo));
+    window.location.reload();
+  }
+
+  useEffect(() => {
+    if (userLogged) {
+      const userData = {
+        ...userLogged,
+        corDeFundo: getCorDeFundoOption(userLogged.corDeFundo),
+      };
+      reset(userData);
+    }
+  }, []);
+
   return (
     <Container>
       <h1>Configurações</h1>
+      <FormContainer onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          name="corDeFundo"
+          control={control}
+          render={({field}) => (
+            <Select
+              {...field}
+              options={optionsBackgroundColor}
+              labelText="Cor de fundo"
+            />
+          )}
+        />
+        <Button text="Salvar alterações" type="submit" />
+      </FormContainer>
     </Container>
   );
 }
--- src/pages/Settings/styles.ts
+++ src/pages/Settings/styles.ts
@@ -7,3 +7,16 @@ export const Container = styled.div`
     margin-left: 4rem;
   }
 `;
+
+export const FormContainer = styled.form`
+  margin-top: 5rem;
+  width: 100%;
+  height: 80%;
+  background: ${(props) => props.theme['white']};
+  padding: 1rem 2.5rem 2rem 1rem;
+  border-radius: 5px;
+
+  div {
+    width: 12rem;
+  }
+`;
